0b540e194c3da5e6c3a2769d1fc0725276a50625
De'Jour <dejourford@yahoo.com>
2025-05-13 18:43:57 -0500

    fix key variables

--- get-buckets.py
+++ get-buckets.py
@@ -1,4 +1,13 @@
-import access_key
+from dotenv import load_dotenv
+import os
 
-print("Access Key: ", access_key.access_key)
-print("Secret Key: ", access_key.secret_key)
+# load environment variables from .env file
+load_dotenv()
+
+# Access keys from environment
+access_key = os.getenv("AWS_ACCESS_KEY_ID")
+secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+
+print("Access Key: ", access_key)
+print("Secret Key: ", secret_key)
